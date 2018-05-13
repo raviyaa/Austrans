52ea1d32835715b012592eda68237ba242acb2fb
ravindu <udayanga.pdlar@gmail.com>
2018-05-13 12:58:39 +0530

    address view edited

--- Telexar.Astrans.WebApi/services/addressService.js
+++ Telexar.Astrans.WebApi/services/addressService.js
@@ -2,7 +2,7 @@
 var connection = require('../connection');
 
 function getListOfAddress(callback) {
-    connection.query('SELECT * from address_books', function (err, results) {
+    connection.query('SELECT * from address_books WHERE user_id=13', function (err, results) {
         if (err) {
             console.log('qwerrty');
             callback(null, JSON.stringify(err));
