564a41976f1fb37fb93e764ee6b4903fbc276306
ravindu <udayanga.pdlar@gmail.com>
2018-05-13 11:42:26 +0530

    now its working properly

--- Telexar.Astrans.WebApi/services/addressService.js
+++ Telexar.Astrans.WebApi/services/addressService.js
@@ -4,6 +4,7 @@ var connection = require('../connection');
 function getListOfAddress(callback) {
     connection.query('SELECT * from address_books', function (err, results) {
         if (err) {
+            console.log('qwerrty');
             callback(null, JSON.stringify(err));
         } else {
             callback(null, JSON.stringify(results));
