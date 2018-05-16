21f2510e4e4222dcb512a3af5c2b8b069435827c
ravindu <udayanga.pdlar@gmail.com>
2018-05-16 15:25:50 +0530

    user profile contd

--- Telexar.Astrans.MobileApp/src/pages/user-profile/user-profile.ts
+++ Telexar.Astrans.MobileApp/src/pages/user-profile/user-profile.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams, ToastController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import * as _ from 'underscore';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -12,7 +13,7 @@ import * as _ from 'underscore';
   templateUrl: 'user-profile.html',
 })
 export class UserProfilePage {
-
+  userPofileForm: FormGroup;
   user: any;
 
   constructor(
@@ -20,6 +21,7 @@ export class UserProfilePage {
     public navParams: NavParams,
     private storage: Storage,
     private astranService: AstranService,
+    private fb: FormBuilder,
     private astronPreloader: AstronPreloader,
     private astronToast: AstronToast
   ) {
@@ -27,19 +29,51 @@ export class UserProfilePage {
 
   ngOnInit() {
     this.getInitData();
+    this.userPofileForm = this.fb.group({
+      companyName: ['', Validators.required],
+      fName: ['', Validators.required],
+      lName: ['', Validators.required],
+      address: ['', Validators.required],
+      email: ['', Validators.required],
+      phone: ['', Validators.required],
+      location: ['', Validators.required],
+      country: ['', Validators.required],
+      /*   state: ['',], */
+      pin: ['', Validators.required],
+      compName: ['', Validators.required],
+      compAddress: ['', Validators.required],
+      compPin: ['', Validators.required]
+    });
   }
   getInitData() {
     this.astronPreloader.show();
     this.storage.get('user').then((user) => {
       this.astronPreloader.hide();
       if (!_.isEmpty(user)) {
-        this.user = JSON.parse(user);
-        this.astronToast.makeToast("Something went dgsdwrong");
+        this.user = JSON.parse(user)[0];
+        this.onUserReceived(this.user);
       } else {
         this.astronToast.makeToast("Something went wrong");
       }
     });
   }
 
- 
+  onUserReceived(user) {
+    this.userPofileForm.patchValue({
+      companyName: this.user.company_name,
+      fName: this.user.first_name,
+      lName: this.user.last_name,
+      address: this.user.address1,
+      email: this.user.email,
+      phone: this.user.phone,
+      location: this.user.city,
+      pin: this.user.postal_code,
+      compName: this.user.company_name,
+      compAddress: this.user.address1 + " " + user.city,
+      compPin: this.user.postal_code
+    });
+  }
+  saveUser() {
+
+  }
 }
