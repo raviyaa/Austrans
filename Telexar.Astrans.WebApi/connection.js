9434204b6ed6295806f20509e62eb16f40960851
ravindu <udayanga.pdlar@gmail.com>
2018-06-05 13:21:43 +0530

    db changed

--- Telexar.Astrans.WebApi/connection.js
+++ Telexar.Astrans.WebApi/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'astrans'
+    database: 'austran_austrans_logistics'
 });
 connection.connect(function (err) {
     if (!err) {
